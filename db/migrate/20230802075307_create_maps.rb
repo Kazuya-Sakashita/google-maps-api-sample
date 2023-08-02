44cd004562ed1e8a11fa9fe14d2bfb25e745d7f5
Kazuya-Sakashita <kz0508@gmail.com>
2023-08-02 16:54:22 +0900

    gem追加、map情報保存テーブル作成

---
+++ db/migrate/20230802075307_create_maps.rb
@@ -0,0 +1,12 @@
+class CreateMaps < ActiveRecord::Migration[7.0]
+  def change
+    create_table :maps do |t|
+      t.references :article, null: false, foreign_key: true
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps
+    end
+  end
+end
